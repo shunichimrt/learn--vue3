a1e36f9ac761e855a2eb1ca1807fc41c5d65ce17
shunichimrt <smurata.official@gmail.com>
2022-05-08 22:05:04 +0900

    top-view

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import List from "@/components/List";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
+    // /→/helloに変更
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/hello",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    // 元々のHelloWorldの3行をそのままコピペしてHelloWorldを→Listに変更
+    {
+      path: "/",
+      name: "List",
+      component: List
     }
   ]
-})
+});
